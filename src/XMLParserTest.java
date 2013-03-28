3d63f83f8dadfcdf7a4a4142ab11bf0fb7d2c231
Karan Goel <karan@goel.im>
2013-03-27 18:27:07 -0700

    download an xml file

--- src/XMLParserTest.java
+++ src/XMLParserTest.java
@@ -1,5 +1,10 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.List;
 
 import org.jdom2.Document;
@@ -7,15 +12,23 @@ import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
-
 public class XMLParserTest {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SAXBuilder builder = new SAXBuilder();
-		File xmlFile = new File("test.xml");
+		String urlToParse = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=6ea9780361e0c7342a8a08a1ad78dfec&artist=cher&track=believe";
+		
+		long start = System.currentTimeMillis();
+		URL website = new URL(urlToParse);
+		ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+		FileOutputStream fos = new FileOutputStream("request.xml");
+		fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+
+		File xmlFile = new File("request.xml");
 
 		try {
 
@@ -34,5 +47,8 @@ public class XMLParserTest {
 		} catch (JDOMException jdomex) {
 			System.out.println(jdomex.getMessage());
 		}
+		long end = System.currentTimeMillis();
+		
+		System.out.println(end - start + " ms");
 	}
 }
